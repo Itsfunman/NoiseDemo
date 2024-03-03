f2fc28b4c96a9745d70b30be64d020a1a30d9e5b
= <=>
2024-03-03 23:31:54 +0100

    General thoughts on calculations

--- world/worldGeneration/PerlinNoise2D.py
+++ world/worldGeneration/PerlinNoise2D.py
@@ -1,7 +1,5 @@
 import random
 
-import numpy
-
 
 class PerlinNoise2D():
 
@@ -25,8 +23,28 @@ class PerlinNoise2D():
             # Iterate over the columns with a step of roundKey
             for j in range(0, len(self.map[i]), round_key):
                 # Modify the element at position [i][j]
-                self.map[i][j] = random.randint(0, 1) * 10
+                self.map[i][j] = random.randint(0, 1)
 
+    def noise(self):
+        for i in range(len(self.map)):
+            for j in range(len(self.map[i])):
+                if j % 20 == 0:
+                    pass
+                else:
+                    self.calculateValue(i,j)
+
+    def calculateValue(self, i, j):
+        # Calculate distance to corner points (j mod 20 = predefined value)
+        # get float based on distance values no clue what to do then
+        # maybe make calculations based on where the point is and the distance
+        # if 8 from point A (being 0) and 12 (being 1) from point B it is 0.4
+        # but how would the second axis affect this?
+        # say it is 5 from C (0) and 15 from D (1) it would be 0.25
+        # How would 0.25 and 0.4 combine?
+
+
+        # Let's just build a coordinate system for now which combines theses valuess
+        pass
 
     def calculate(self):
         pass
